ea326304e6bada0958e3b3903693705d8ce5f139
kjwon15 <kjwonmail@gmail.com>
2014-04-06 15:00:10 +0900

    ActionBar goto up

--- src/main/java/kai/twitter/voice/SettingsActivity.java
+++ src/main/java/kai/twitter/voice/SettingsActivity.java
@@ -2,12 +2,17 @@ package kai.twitter.voice;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.app.ActionBar;
 
 public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
         addPreferencesFromResource(R.xml.prefs);
     }
 }
