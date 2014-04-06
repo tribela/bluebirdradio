6283dd0f34c20b6afa18e3fc0cbe30a0367cd614
kjwon15 <kjwonmail@gmail.com>
2014-04-06 16:05:09 +0900

    actionbar compat

--- src/main/java/kai/twitter/voice/SettingsActivity.java
+++ src/main/java/kai/twitter/voice/SettingsActivity.java
@@ -1,5 +1,6 @@
 package kai.twitter.voice;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.app.ActionBar;
@@ -10,8 +11,10 @@ public class SettingsActivity extends PreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ActionBar actionBar = getActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            ActionBar actionbar = getActionBar();
+            actionbar.setDisplayHomeAsUpEnabled(true);
+        }
 
         addPreferencesFromResource(R.xml.prefs);
     }
