78b0da17dc67035a60079cdcf81267be8a49b6b1
kjwon15 <kjwonmail@gmail.com>
2014-05-05 19:53:02 +0900

    Notes for licenses.

---
+++ TwitterVoice/icons.sh
@@ -0,0 +1,5 @@
+convert src/main/ic_launcher-web.png -resize 48x48 src/main/res/drawable-mdpi/ic_launcher.png
+convert src/main/ic_launcher-web.png -resize 72x72 src/main/res/drawable-hdpi/ic_launcher.png
+convert src/main/ic_launcher-web.png -resize 96x96 src/main/res/drawable-xhdpi/ic_launcher.png
+convert src/main/ic_launcher-web.png -resize 96x96 src/main/res/drawable-xhdpi/ic_launcher.png
+convert src/main/ic_launcher-web.png -resize 144x144 src/main/res/drawable-xxhdpi/ic_launcher.png
---
+++ TwitterVoice/src/main/java/kai/twitter/voice/ViewLicenses.java
@@ -0,0 +1,42 @@
+package kai.twitter.voice;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ViewLicenses extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_licenses);
+
+        TextView headerView = (TextView) findViewById(R.id.licensesHeaderView);
+        TextView mainView = (TextView) findViewById(R.id.licensesMainView);
+
+
+        readText("notes.txt", headerView);
+        readText("LICENSE.txt", mainView);
+    }
+
+    private void readText(String filename, TextView headerView) {
+        try {
+            InputStream noteIn = getAssets().open(filename);
+            int size = noteIn.available();
+            byte[] buffer = new byte[size];
+            noteIn.read(buffer);
+            noteIn.close();
+            String text = new String(buffer);
+            headerView.setText(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("READ", e.getMessage());
+        }
+    }
+}
