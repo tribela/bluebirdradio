6b7493539d56beee4abbed5b0f46684be600197b
kjwon15 <kjwonmail@gmail.com>
2014-05-18 03:51:31 +0900

    Remove unused import.

--- TwitterVoice/src/main/java/kai/twitter/voice/TwitterVoiceService.java
+++ TwitterVoice/src/main/java/kai/twitter/voice/TwitterVoiceService.java
@@ -16,8 +16,6 @@ import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
