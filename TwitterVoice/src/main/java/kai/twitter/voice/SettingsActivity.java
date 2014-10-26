380d7d9718f7913242b4abb564fb7d9da054a607
kjwon15 <kjwonmail@gmail.com>
2014-10-26 20:27:31 +0900

    Settings using fragment

--- TwitterVoice/src/main/java/kai/twitter/voice/SettingsActivity.java
+++ TwitterVoice/src/main/java/kai/twitter/voice/SettingsActivity.java
@@ -5,24 +5,24 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
 
-public class SettingsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class SettingsActivity extends ActionBarActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            ActionBar actionbar = getActionBar();
-            actionbar.setDisplayHomeAsUpEnabled(true);
-        }
-
         SharedPreferences preference = PreferenceManager.getDefaultSharedPreferences(this);
         preference.registerOnSharedPreferenceChangeListener(this);
 
-        addPreferencesFromResource(R.xml.prefs);
+        getFragmentManager()
+        .beginTransaction()
+        .replace(android.R.id.content, new SettingsFragment())
+        .commit();
+
     }
 
     @Override
@@ -31,5 +31,14 @@ public class SettingsActivity extends PreferenceActivity implements SharedPrefer
         intent.putExtra("KEY", key);
         intent.setAction(getResources().getString(R.string.ACTION_CHANGE_PREFERENCE));
         sendBroadcast(intent);
+    }
+
+    private class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(R.xml.prefs);
+        }
     }
 }
